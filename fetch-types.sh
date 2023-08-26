fdafff74e3d79a9f45a44597883ef59fa19903c5
ahmed-5g <ahmed_5g@yahoo.com>
2023-08-27 00:57:59 +0300

    feat: hook 6

--- fetch-types.sh
+++ fetch-types.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Go to the submodule directory
-cd shared-types
+cd ./shared-types
 
 # Path to the git executable
 # GIT_PATH=/usr/bin/git
 
 # Fetch the latest changes from the remote repository
-git pull
+git pull origin/master
 
+git merge origin/master
 # Merge the remote changes with your local version
 # $GIT_PATH merge origin/master

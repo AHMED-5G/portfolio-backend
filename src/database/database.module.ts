ff83075c1234c338c72337c41e6299a9425b94a9
ahmed-5g <ahmed_5g@yahoo.com>
2023-08-26 09:50:19 +0300

    fix: configs is req

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,20 +10,23 @@ console.log('database.module.ts  -> ', process.env.NODE_ENV);
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
-        const mysqlConfig = {
-          type: 'mysql' as const,
+        const commonConfig = {
           host: configService.getOrThrow('DB_HOST'),
           port: configService.getOrThrow('DB_PORT'),
           database: configService.getOrThrow('DB_NAME'),
           username: configService.getOrThrow('DB_USERNAME'),
           password: configService.getOrThrow('DB_PASSWORD'),
+        };
+        const mysqlConfig = {
+          type: 'mysql' as const,
+          ...commonConfig,
           autoLoadEntities: true,
           synchronize: process.env.NODE_ENV === 'development',
         };
 
         const postgressConfig = {
           type: 'postgres' as const,
-          // ...commonConfig,
+          ...commonConfig,
           synchronize: false,
         };
 

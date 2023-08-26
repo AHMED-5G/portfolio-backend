089b7d2717b5a987d8d3754c884ced80f1b2634f
ahmed-5g <ahmed_5g@yahoo.com>
2023-08-27 00:48:03 +0300

    feat: action test 4

---
+++ fetch-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Go to the submodule directory
+cd shared-types
+
+# Path to the git executable
+GIT_PATH=/usr/bin/git
+
+# Fetch the latest changes from the remote repository
+$GIT_PATH fetch
+
+# Merge the remote changes with your local version
+$GIT_PATH merge origin/master

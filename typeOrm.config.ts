4994f483568ad5637b3119f5325f51dfdfe3f9d7
ahmed-5g <ahmed_5g@yahoo.com>
2023-08-26 09:13:48 +0300

    fix: db no config for postgress

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -22,11 +22,11 @@ const mysqlConfig: DataSourceOptions = {
 
 const postgressConfig: DataSourceOptions = {
   type: 'postgres',
-  host: configService.getOrThrow('DB_HOST').toString(),
-  port: configService.getOrThrow('DB_PORT').toString(),
-  database: configService.getOrThrow('DB_NAME').toString(),
-  username: configService.getOrThrow('DB_USERNAME').toString(),
-  password: configService.getOrThrow('DB_PASSWORD').toString(),
+  // host: configService.getOrThrow('DB_HOST').toString(),
+  // port: configService.getOrThrow('DB_PORT').toString(),
+  // database: configService.getOrThrow('DB_NAME').toString(),
+  // username: configService.getOrThrow('DB_USERNAME').toString(),
+  // password: configService.getOrThrow('DB_PASSWORD').toString(),
   migrations: ['migrations/**'],
   entities: [User, ResetToken],
 };

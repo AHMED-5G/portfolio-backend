9a6250f4427593a3f2e73a976828b93abe6cb8dd
ahmed-5g <ahmed_5g@yahoo.com>
2023-09-06 01:35:06 +0300

    chore: prepare for first production ,db config

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -7,17 +7,17 @@ config();
 
 const configService = new ConfigService();
 
-const mysqlConfig: DataSourceOptions = {
-  type: "mysql",
-  host: configService.getOrThrow("SQL_DB_HOST"),
-  port: configService.getOrThrow("SQL_DB_PORT"),
-  database: configService.getOrThrow("SQL_DB_NAME"),
-  username: configService.getOrThrow("SQL_DB_USERNAME"),
-  password: configService.getOrThrow("SQL_DB_PASSWORD"),
-  synchronize: true,
-  migrations: ["dist/migrations/**"],
-  entities: ["dist/**/*.entity.js"],
-};
+// const mysqlConfig: DataSourceOptions = {
+//   type: "mysql",
+//   host: configService.getOrThrow("SQL_DB_HOST"),
+//   port: configService.getOrThrow("SQL_DB_PORT"),
+//   database: configService.getOrThrow("SQL_DB_NAME"),
+//   username: configService.getOrThrow("SQL_DB_USERNAME"),
+//   password: configService.getOrThrow("SQL_DB_PASSWORD"),
+//   synchronize: true,
+//   migrations: ["dist/migrations/**"],
+//   entities: ["dist/**/*.entity.js"],
+// };
 
 const postgressConfig: DataSourceOptions = {
   type: "postgres",
@@ -33,7 +33,8 @@ const postgressConfig: DataSourceOptions = {
 };
 
 console.log("typeOrm.config.ts -> ", process.env.NODE_ENV);
-export const myDataSourceOptions: DataSourceOptions =
-  process.env.NODE_ENV === "development" ? mysqlConfig : postgressConfig;
+export const myDataSourceOptions: DataSourceOptions = postgressConfig;
+// export const myDataSourceOptions: DataSourceOptions =
+  // process.env.NODE_ENV === "development" ? mysqlConfig : postgressConfig;
 
 export default new DataSource(myDataSourceOptions);
